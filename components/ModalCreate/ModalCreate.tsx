435ab2e0c0e6677d6de64b6d9b39fcaa34d16918
Yuri <davydiuk.yurii@gmail.com>
2026-05-03 17:20:12 +0300

    fix: modal logic

--- components/ModalCreate/ModalCreate.tsx
+++ components/ModalCreate/ModalCreate.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { createPortal } from 'react-dom';
 import css from './Modal.module.css';
 import { useEffect } from 'react';
 
@@ -32,10 +31,9 @@ export default function ModalCreate({ onClose, children }: ModalProps) {
     };
   }, [onClose]);
 
-  return createPortal(
+  return (
     <div className={css.backdrop} role="dialog" aria-modal="true" onClick={handleBackdropClick}>
       <div className={css.modal}>{children}</div>
-    </div>,
-    document.body
+    </div>
   );
 }
